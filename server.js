71c7726629213a558cd76902631ab1a28ed4e046
Kimberly Salas <salask0104@gmail.com>
2021-06-07 16:33:12 -0700

    added to the server.js - throws a 404 when not on port 3001

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
