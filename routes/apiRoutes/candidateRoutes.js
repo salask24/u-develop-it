6312b3a38618d7d3d8afe6285ebfce47094b019a
Kimberly Salas <salask0104@gmail.com>
2021-06-14 05:46:22 -0700

    fixed thedb path

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../db/connection');
+const db = require('../../connection');
 const inputCheck = require('../../utils/inputCheck');
 
 
--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../db/connection');
+const db = require('../../connection');
 
 // Get all parties
 router.get('/parties', (req, res) => {
